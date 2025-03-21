091f2eed4a35ed1c5ce4fbde76a97a0167bfe89a
Art <aironer@yandex.ru>
2025-03-21 16:51:32 +0300

    Реализация функции скрытия блока со страницы

--- Progress-project/Progress.js
+++ Progress-project/Progress.js
@@ -1,1 +1,24 @@
-// 1
+function test() {
+  const progressBlock = document.getElementById("progress-block")
+
+  const hideBox = document.getElementById("hideBox")
+
+  hideBox.addEventListener("change", function () {
+    console.log(event.target.checked)
+    if (event.target.checked) {
+      hideProgress()
+    } else {
+      showProgress()
+    }
+  })
+
+  function hideProgress() {
+    progressBlock.style.display = "none"
+  }
+
+  function showProgress() {
+    progressBlock.style.display = "block"
+  }
+}
+
+test()
