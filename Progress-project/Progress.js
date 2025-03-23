ceb349bc0a601b647747f146bb183f9504ec9d5e
Art <aironer@yandex.ru>
2025-03-23 13:04:08 +0300

    Оптимизация кода для переиспользования. 1. Добавлены ограничения для ввода числа. 2. Реализация на классах и методах.

--- Progress-project/Progress.js
+++ Progress-project/Progress.js
@@ -1,50 +1,86 @@
-function test() {
-  const progressBlock = document.getElementById("box")
-  const hideBox = document.getElementById("hideBox")
-  const loaderElement = document.getElementById("loader")
-  const valueInput = document.getElementById("valueInput")
-  const animateToggle = document.getElementById("animateToggle")
+class ProgressComponent {
+  constructor(container) {
+    this.container = container
+    this.valueInput = null
+    this.hideBox = null
+    this.animateToggle = null
+    this.loaderElement = null
 
-  const loaderAPI = {
-    setValue: function (value) {
-      if (value < 0 || value > 100) {
-        console.log("Значение должно быть от 0 до 100")
-        return
-      }
-
-      const degree = (value / 100) * 360
-      loaderElement.style.transform = `rotate(${degree - 43}deg)`
-    },
-    setAnimated: function (isAnimated) {
-      if (isAnimated) {
-        loaderElement.style.animation = "spin 2s linear infinite"
-      } else {
-        loaderElement.style.animation = "none"
-      }
-    }
+    this.render()
+    this.bindEvents()
   }
 
-  valueInput.addEventListener("input", function () {
-    const value = valueInput.value === "" ? 0 : parseInt(valueInput.value)
-    if (value > 100) {
-      valueInput.style.color = "red"
-    } else if (value < 100) {
-      valueInput.style.color = ""
-    }
-    loaderAPI.setValue(value)
-  })
+  render() {
+    this.container.innerHTML = `
+        <div class="progress">
+            <div class="progress__block" id="box">
+                <div id="loader" class="progress__block-loader"></div>
+            </div>
+            <div class="progress__control-panel">
+                <label class="progress__toggle">
+                    <input type="number" id="valueInput" min="0" max="100" placeholder="...">Value
+                </label>
+                <label class="progress__toggle">
+                    <input type="checkbox" id="animateToggle">
+                    <span class="switch"></span> Animate
+                </label>
+                <label class="progress__toggle">
+                    <input type="checkbox" id="hideBox">
+                    <span class="switch"></span> Hide
+                </label>
+            </div>
+        </div>
+      `
 
-  animateToggle.addEventListener("change", function () {
-    loaderAPI.setAnimated(animateToggle.checked)
-  })
+    this.valueInput = this.container.querySelector("#valueInput")
+    this.hideBox = this.container.querySelector("#hideBox")
+    this.animateToggle = this.container.querySelector("#animateToggle")
+    this.loaderElement = this.container.querySelector("#loader")
+  }
 
-  hideBox.addEventListener("change", function (event) {
-    if (event.target.checked) {
-      progressBlock.style.display = "none"
-    } else {
-      progressBlock.style.display = "block"
+  bindEvents() {
+    this.valueInput.addEventListener("input", () => {
+      let value = parseInt(this.valueInput.value)
+      if (isNaN(value)) value = 0
+
+      if (value < 0) {
+        value = 0
+      } else if (value > 100) {
+        value = 100
+      }
+
+      this.valueInput.value = value
+      this.setValue(value)
+    })
+
+    this.animateToggle.addEventListener("change", () => {
+      this.setAnimated(this.animateToggle.checked)
+    })
+
+    this.hideBox.addEventListener("change", (event) => {
+      this.toggleVisibility(event.target.checked)
+    })
+  }
+
+  setValue(value) {
+    if (value < 0 || value > 100) {
+      console.log("Значение должно быть от 0 до 100")
+      return
     }
-  })
+    const degree = (value / 100) * 360
+    this.loaderElement.style.transform = `rotate(${degree - 43}deg)`
+  }
+
+  setAnimated(isAnimated) {
+    this.loaderElement.style.animation = isAnimated ? "spin 2s linear infinite" : "none"
+  }
+
+  toggleVisibility(isHidden) {
+    this.container.querySelector(".progress__block").style.display = isHidden
+      ? "none"
+      : "block"
+  }
 }
 
-test()
+const container = document.getElementById("progressComponent")
+const progressComponent = new ProgressComponent(container)
